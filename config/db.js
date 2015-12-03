3612f423716ae3e0ec1eefd8e08d585cbdb3dd79
chris netten <200230280@MyGeorgian.ca>
2015-12-02 20:37:43 -0500

    .

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ module.exports = {
  
 //LOCAL
 //'url':'mongodb://localhost/userDB'
-
+//
 //LIVE	
-'url' : ' mongodb://cnetten:surveydb@ds057944.mongolab.com:57944/surveydb'
+'url' : 'mongodb://cnetten:surveydb@ds057944.mongolab.com:57944/surveydb'
 
 };
