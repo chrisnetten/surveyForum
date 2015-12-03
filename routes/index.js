fc7849b83a6d222bcf08b5933df258a19805e3bd
chris netten <200230280@MyGeorgian.ca>
2015-12-02 20:43:01 -0500

    add user model

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
