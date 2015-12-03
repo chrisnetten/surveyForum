ca47b45c643edb45b4fb52863f1e79041dd9a9c4
chris netten <200230280@MyGeorgian.ca>
2015-12-02 20:51:28 -0500

    flash message

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', {
     title: 'Home', 
+     messages: req.flash('loginMessage'),
     displayName: req.user ? req.user.displayName : ''
   })
 });
