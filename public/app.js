881d963caa5562edb2d129fad887baf5f6392289
chris netten <200230280@MyGeorgian.ca>
2015-12-13 16:47:38 -0500

    bower - angular - local

---
+++ public/app.js
@@ -0,0 +1,4 @@
+/// <reference path="_reference.ts" />
+var mainModuleName = "app";
+var app = angular.module(mainModuleName, []);
+//# sourceMappingURL=app.js.map
---
+++ public/app.ts
@@ -0,0 +1,5 @@
+/// <reference path="_reference.ts" />
+
+var mainModuleName = "app";
+
+var app = angular.module(mainModuleName, []);
