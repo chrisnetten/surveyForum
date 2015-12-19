c9d98f85b998672f44535495578a4683ef036a4c
chris netten <200230280@MyGeorgian.ca>
2015-12-18 23:10:37 -0500

    .

--- public/app.ts
+++ public/app.ts
@@ -78,68 +78,7 @@
             };
         }]);
         
-         app.controller('UserSurveyController', ['$scope', 'Survey', function ($scope, Survey) {
-            $scope.editing = [];
-            $scope.username = '';
-         
-            $scope.userSurvey = [];
-            $scope.setUserName = function (userName) {
-                $scope.username = userName; //get the username
-                $scope.survey = Survey.query(function () {
-                    $scope.userSurvey = []; // reset the userTodos array
-                    $scope.survey.forEach(function (survey) {
-                        
-                        
-                            $scope.userSurvey.push(survey);
-                        
-                    });
-                    $scope.survey = $scope.userSurvey;
-                });
-            };
-            $scope.save = function () {
-                if (!$scope.newSurvey , !$scope.newQuestion, !$scope.newDescription|| $scope.newSurvey.length < 1, $scope.newQuestion.length < 1, $scope.newDescription.length < 1) {
-                    return;
-                }
-                var survey = new Survey({ name: $scope.newSurvey, question: $scope.newQuestion, description: $scope.newDescription,username: $scope.username, completed: false });
-                survey.$save(function () {
-                    $scope.survey.push(survey);
-                    $scope.newSurvey = '';
-                    $scope.newQuestion = ''; // clear textbox
-                    $scope.newDescription = '';
-                });
-            };
-            $scope.update = function (index) {
-                var survey = $scope.survey[index];
-                Survey.update({ id: survey._id }, survey);
-                $scope.editing[index] = false;
-            };
-            $scope.edit = function (index) {
-                
-                $scope.editing[index] = angular.copy($scope.survey[index]);
-                
-            };
-            $scope.cancel = function (index) {
-                $scope.survey[index] = angular.copy($scope.editing[index]);
-                $scope.editing[index] = false;
-            };
-            $scope.remove = function (index) {
-                var survey = $scope.survey[index];
-                Survey.remove({ id: survey._id }, function () {
-                    $scope.survey.splice(index, 1);
-                });
-                $scope.editing[index] = false;
-            };
-           
-            $scope.totalSurvey = function () {
-                var count = 0;
-                angular.forEach($scope.survey, function (survey) {
-                   
-                        count++;
-                    
-                });
-                return count;
-            };
-        }]);
+       
         
 
          
@@ -202,7 +141,7 @@
             })
                 .when('/userSurvey', {
                 templateUrl: '/userSurvey.html',
-                controller: 'UserSurveyController' ,
+                controller: 'SurveyController' ,
             });
         }]);
 })();
