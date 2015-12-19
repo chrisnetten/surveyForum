4d0e194a30c7b7ccdb06aa20ec6c684af4de800f
chris netten <200230280@MyGeorgian.ca>
2015-12-18 22:57:49 -0500

    .

--- public/app.ts
+++ public/app.ts
@@ -9,12 +9,80 @@
     });
     
     app.factory('Survey', ['$resource', function ($resource) {
-            return $resource('/survey/:id', '/userSurvey/:id',null, {
+            return $resource('/survey/:id',null, {
+                'update': { method: 'PUT' }
+            });
+        }]);
+        app.factory('userSurvey', ['$resource', function ($resource) {
+            return $resource('/userSurvey/:id',null, {
                 'update': { method: 'PUT' }
             });
         }]);
     
      app.controller('SurveyController', ['$scope', 'Survey', function ($scope, Survey) {
+            $scope.editing = [];
+            $scope.username = '';
+         
+            $scope.userSurvey = [];
+            $scope.setUserName = function (userName) {
+                $scope.username = userName; //get the username
+                $scope.survey = Survey.query(function () {
+                    $scope.userSurvey = []; // reset the userTodos array
+                    $scope.survey.forEach(function (survey) {
+                        
+                        
+                            $scope.userSurvey.push(survey);
+                        
+                    });
+                    $scope.survey = $scope.userSurvey;
+                });
+            };
+            $scope.save = function () {
+                if (!$scope.newSurvey , !$scope.newQuestion, !$scope.newDescription|| $scope.newSurvey.length < 1, $scope.newQuestion.length < 1, $scope.newDescription.length < 1) {
+                    return;
+                }
+                var survey = new Survey({ name: $scope.newSurvey, question: $scope.newQuestion, description: $scope.newDescription,username: $scope.username, completed: false });
+                survey.$save(function () {
+                    $scope.survey.push(survey);
+                    $scope.newSurvey = '';
+                    $scope.newQuestion = ''; // clear textbox
+                    $scope.newDescription = '';
+                });
+            };
+            $scope.update = function (index) {
+                var survey = $scope.survey[index];
+                Survey.update({ id: survey._id }, survey);
+                $scope.editing[index] = false;
+            };
+            $scope.edit = function (index) {
+                
+                $scope.editing[index] = angular.copy($scope.survey[index]);
+                
+            };
+            $scope.cancel = function (index) {
+                $scope.survey[index] = angular.copy($scope.editing[index]);
+                $scope.editing[index] = false;
+            };
+            $scope.remove = function (index) {
+                var survey = $scope.survey[index];
+                Survey.remove({ id: survey._id }, function () {
+                    $scope.survey.splice(index, 1);
+                });
+                $scope.editing[index] = false;
+            };
+           
+            $scope.totalSurvey = function () {
+                var count = 0;
+                angular.forEach($scope.survey, function (survey) {
+                   
+                        count++;
+                    
+                });
+                return count;
+            };
+        }]);
+        
+        app.controller('UserSurveyController', ['$scope', 'userSurvey', function ($scope, Survey) {
             $scope.editing = [];
             $scope.username = '';
          
@@ -136,7 +204,7 @@
             })
                 .when('/userSurvey', {
                 templateUrl: '/userSurvey.html',
-                controller: 'SurveyController' ,
+                controller: 'UserSurveyController' ,
             });
         }]);
 })();
